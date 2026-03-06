149443e76a2dc6fef4d1fa40ea3be37b76695f16
Bhumica-jaiswal <bhumicajaiswal@gmail.com>
2026-03-06 14:25:57 +0530

    Add Mission class and update Drone implementation

--- include/Mission.h
+++ include/Mission.h
@@ -1,12 +1,27 @@
 #pragma once
 
+#include <string>
 #include "Drone.h"
+
+enum class MissionType {
+    Delivery,
+    Surveillance,
+    Inspection
+};
 
 class Mission {
 public:
-    Mission(Point destination);
+    Mission(std::string id, MissionType type, Point destination, int priority);
+
+    std::string getMissionDetails() const;
     Point getDestination() const;
+    int getPriority() const;
+    MissionType getType() const;
+    const std::string& getId() const;
 
 private:
+    std::string id_;
+    MissionType type_;
     Point destination_;
+    int priority_;
 };
--- src/Mission.cpp
+++ src/Mission.cpp
@@ -1,5 +1,23 @@
 #include "Mission.h"
+#include <sstream>
 
-Mission::Mission(Point destination) : destination_(destination) {}
+Mission::Mission(std::string id, MissionType type, Point destination, int priority)
+    : id_(std::move(id)), type_(type), destination_(destination), priority_(priority) {}
+
+std::string Mission::getMissionDetails() const {
+    std::ostringstream oss;
+    oss << "[" << id_ << "] ";
+    switch (type_) {
+        case MissionType::Delivery:    oss << "Delivery"; break;
+        case MissionType::Surveillance: oss << "Surveillance"; break;
+        case MissionType::Inspection:   oss << "Inspection"; break;
+    }
+    oss << " to (" << destination_.x << "," << destination_.y << ") ";
+    oss << "prio=" << priority_;
+    return oss.str();
+}
 
 Point Mission::getDestination() const { return destination_; }
+int Mission::getPriority() const { return priority_; }
+MissionType Mission::getType() const { return type_; }
+const std::string& Mission::getId() const { return id_; }
